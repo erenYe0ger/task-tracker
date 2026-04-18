4685b64cd8e8b1c91e4d8042ae955b750f8d142c
Goutam Mukherjee <quothyparser97@gmail.com>
2026-04-18 13:33:14 +0530

    feat: add get & post endpoint for taskList using services & controllers

---
+++ backend/tasks/src/main/java/com/example/tasks/services/impl/TaskListServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.tasks.services.impl;
+
+import com.example.tasks.domain.entities.TaskList;
+import com.example.tasks.repositories.TaskListRepository;
+import com.example.tasks.services.TaskListService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class TaskListServiceImpl implements TaskListService {
+
+    private final TaskListRepository taskListRepository;
+
+    public TaskListServiceImpl(TaskListRepository taskListRepository) {
+        this.taskListRepository = taskListRepository;
+    }
+
+
+    @Override
+    public List<TaskList> listTaskLists() {
+        return taskListRepository.findAll();
+    }
+
+    @Override
+    public TaskList createTaskList(TaskList taskList) {
+
+        if(taskList.getId() != null) {
+            throw new IllegalArgumentException("TaskList already has an ID!");
+        }
+
+        if(taskList.getTitle() == null || taskList.getTitle().isEmpty()) {
+            throw new IllegalArgumentException("No title provided!");
+        }
+
+
+        LocalDateTime now = LocalDateTime.now();
+
+        TaskList createdTaskList = new TaskList(
+                null,
+                taskList.getTitle(),
+                taskList.getDescription(),
+                null,
+                now,
+                now
+        );
+
+        return taskListRepository.save(createdTaskList);
+    }
+}
